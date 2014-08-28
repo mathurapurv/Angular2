f1793efca66704be7e0bb45de72b80436e04e70b
apurv <apurv@apurv-PC>
2014-08-29 01:46:28 +0530

    kjhkh

--- AngularDemo-test_branch/web/app/js/new-services/ProfilePicUrlBuilder.js
+++ AngularDemo-test_branch/web/app/js/new-services/ProfilePicUrlBuilder.js
@@ -1,27 +1,31 @@
 'use strict';
 
 eventsApp.factory('profilePicBuilder' , function($http, $log){
-	return {
-		buildProfilePicUrl : function(picName){
-			return 'img/'+picName+'.png';
-		},
-		
-		fetchUserInfo : function(userName , successcb){
-			$http({method : 'GET' , url : 'http://localhost:8080/TestGrails/test/obtainUserInfo?userName='+userName}).
-			  success( function(data, status, headers, config){
-				  alert('data'+data);
-				  successcb(data);
-			  }).
-			    error( function(data, status, headers, config){
-			    	alert('error');
-			    	$log.info(data, status, headers, config);
-			    });
-			
-		}
-		
-		
-		
-		
-		
-	};
+		return {
+						buildProfilePicUrl : function(picName) {
+							return 'img/' + picName + '.png';
+						},
+
+						fetchUserInfo : function(userName, successcb) {
+
+							if (userName.length > 2) {
+								$http(
+										{
+											method : 'GET',
+											url : 'http://localhost:8080/TestGrails/test/obtainUserInfo?userName='
+													+ userName
+										})
+										.success(
+												function(data, status, headers,	config) {
+													if (data.userName != null)successcb(data);
+												}).error(
+												function(data, status, headers,config) {
+													alert('error');
+													$log.info(data, status,headers, config);
+												});
+							}
+
+						}
+
+					};
 });
